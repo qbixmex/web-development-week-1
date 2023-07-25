948e7e2c52e02e51983e39caa4fb21fd32cd0a77
Daniel González Briseño <qbixmex@gmail.com>
2023-07-25 14:34:48 -0700

    third-exercise

--- exercises/ex03.js
+++ exercises/ex03.js
@@ -8,11 +8,28 @@ Create a function named numberOfVowels that will receive a string and return the
 */
 
 const numberOfVowels = function (data) {
-  // Put your solution here
+  const vowels = ["a", "e", "i", "o", "u"];
+  const dataArray = [];
+  let count = 0;
+
+  
+  for (character of data) {
+    dataArray.push(character);
+  }
+
+  vowels.forEach(vowel => {
+    dataArray.forEach(character => {
+      if (vowel === character) {
+        count++;
+      }
+    });
+  });
+
+  return count;
 };
 
-console.log(numberOfVowels("orange")); // 3
-console.log(numberOfVowels("cornerstone college")); // 7
-console.log(numberOfVowels("aeiou")); // 5
+// console.log(numberOfVowels("orange")); // 3
+// console.log(numberOfVowels("cornerstone college")); // 7
+// console.log(numberOfVowels("aeiou")); // 5
 
 module.exports = numberOfVowels;
